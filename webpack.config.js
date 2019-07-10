10749d8d19f7cbd52b21c499926aa4bb2d219d37
Piotr Obrebowski <Piotr.Obrebowski@cloudexpert.pl>
2019-07-10 12:48:08 +0200

    first

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use : [
+                    {
+                        loader: "html-loader",
+                        options : {minimize : true}
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    }, 
+    plugins : [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html" 
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename : "[id].css"
+        })
+
+    ]
+}
